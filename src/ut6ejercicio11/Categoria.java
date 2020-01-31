504c42094c3ea4e3b67de6a9b8820ce405f75dd4
dam <dam@172.26.81.176>
2020-01-31 10:17:18 +0100

    AñadidosComentarios

--- src/ut6ejercicio11/Categoria.java
+++ src/ut6ejercicio11/Categoria.java
@@ -10,19 +10,31 @@ package ut6ejercicio11;
  * @author dam
  */
 public class Categoria {
-    
+    /**
+     * atributos de la clase categoria
+     */
     private String nombre;
     private float sueldo;
-
+    /**
+     * constructor de la clase categoria
+     * @param nombre le pasaremos el nombre cuando le creemos
+     * @param sueldo le pasaremos el sueldo cuando le creemos
+     */
     public Categoria(String nombre, float sueldo) {
         this.nombre = nombre;
         this.sueldo = sueldo;
     }
-
+    /**
+     * metodo para poder acceder al nombre
+     * @return retornamos el nombre del empleado
+     */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * metodo para poder acceder al sueldo
+     * @return retornamos el sueldo del empleado
+     */
     public float getSueldo() {
         return sueldo;
     }
